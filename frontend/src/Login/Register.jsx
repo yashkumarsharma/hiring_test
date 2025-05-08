4dbb4cb03c99cf85a49ea073eed99878cb2a0112
Yash Kumar Sharma <yash@raenabeauty.com>
2025-05-09 03:10:43 +0400

    feat: update Register page UI

--- frontend/src/Login/Register.jsx
+++ frontend/src/Login/Register.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
+
+import backgroundGif from "../assets/images/play.gif";
+import GameTitle from '../components/GameTitle';
 import styles from './Register.module.css';
 
 const Register = () => {
@@ -17,30 +20,38 @@ const Register = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <form onSubmit={handleSubmit}>
-        <h2>Register</h2>
-        <input
-          type="text"
-          placeholder="Username"
-          value={formData.username}
-          onChange={(e) => setFormData({ ...formData, username: e.target.value })}
-          className={styles.input}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={formData.password}
-          onChange={(e) => setFormData({ ...formData, password: e.target.value })}
-          className={styles.input}
-        />
-        <div className="login-button-container">
-          <button type="submit" className={"game-button"}>
-            Register
-          </button>
-        </div>
-        {message && <p className={styles.message}>{message}</p>}
-      </form>
+    <div
+      className="background-container"
+      style={{
+        backgroundImage: `url(${backgroundGif})`,
+      }}
+    >
+      <GameTitle />
+      <div className={styles.container}>
+        <form onSubmit={handleSubmit}>
+          <h2>Register</h2>
+          <input
+            type="text"
+            placeholder="Username"
+            value={formData.username}
+            onChange={(e) => setFormData({ ...formData, username: e.target.value })}
+            className={styles.input}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+            className={styles.input}
+          />
+          <div className="login-button-container">
+            <button type="submit" className={"game-button"}>
+              Register
+            </button>
+          </div>
+          {message && <p className={styles.message}>{message}</p>}
+        </form>
+      </div>
     </div>
   );
 };
